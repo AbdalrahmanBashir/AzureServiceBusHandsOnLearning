1f988757f3f23f95c8a1ad32b7c6b43e96929ee2
Abdalrahman H Bashir <127059452+AbdalrahmanBashir@users.noreply.github.com>
2025-09-14 19:41:43 -0500

    Adding topics, and subscriptions example.

---
+++ EmailServiceConsumer/Program.cs
@@ -0,0 +1,69 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text.Json;
+
+public record User(Guid UserId, string FullName, string Email);
+public class Program
+{
+    private const string ServiceBusConnectionString = "Endpoint=sb://pub-sub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=McN7wxXv/EbavH/OHLrWQ17PILkVOD3S4+ASbKQEHlY=";
+
+    private const string TopicName = "UserRegistered";
+
+    private const string SubscriptionName = "EmailServiceSubscription";
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine($"Initializing Email Service Consumer for subscription '{SubscriptionName}'...");
+        await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
+        ServiceBusProcessor processor = client.CreateProcessor(TopicName, SubscriptionName, new ServiceBusProcessorOptions());
+
+        processor.ProcessMessageAsync += MessageHandler;
+
+        processor.ProcessErrorAsync += ErrorHandler;
+
+        await processor.StartProcessingAsync();
+
+        Console.WriteLine("Processor started. Listening for user registration events. Press any key to stop.");
+        Console.ReadKey();
+
+        Console.WriteLine("\nStopping the message processor...");
+        await processor.StopProcessingAsync();
+        Console.WriteLine("Processor stopped.");
+    }
+
+    static  Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Error: {args.Exception.Message}");
+        Console.ResetColor();
+        return Task.CompletedTask;
+    }
+
+    private static async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        string body = args.Message.Body.ToString();
+        Console.WriteLine($"[Received raw message]: {body}");
+
+        try
+        {
+
+            var user = JsonSerializer.Deserialize<User>(body);
+
+            if (user != null)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[Email Service] | Sending welcome email to {user.FullName} at {user.Email}...");
+                Console.ResetColor();
+            }
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+        catch (JsonException jsonEx)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error deserializing message: {jsonEx.Message}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ TopicPublisher/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using Azure.Messaging.ServiceBus;
+using System.Text.Json;
+
+public class Program
+{
+    private const string ServiceBusConnectionString = "Endpoint=sb://pub-sub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=McN7wxXv/EbavH/OHLrWQ17PILkVOD3S4+ASbKQEHlY=";
+
+    private const string TopicName = "UserRegistered";
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Initializing Service Bus client and sender for the topic...");
+        await using var client = new ServiceBusClient(ServiceBusConnectionString);
+        await using var sender = client.CreateSender(TopicName);
+
+        Console.WriteLine("Creating and sending messages with application properties...");
+
+        var users = new[]
+        {
+            new {UserId = Guid.NewGuid(), FullName = "Adam Brown", Email = "Adam@bashir.com"},
+            new {UserId = Guid.NewGuid(), FullName = "Adam Williams", Email = "Williams@bashir.com"},
+            new {UserId = Guid.NewGuid(), FullName = "Williams Brown", Email = "Brown@bashir.com"}
+        };
+
+        var messagesToSend = new List<ServiceBusMessage>();
+        foreach (var user in users)
+        {
+            string messageBody = JsonSerializer.Serialize(user);
+            var message =  new ServiceBusMessage(messageBody);
+            message.ApplicationProperties.Add("EventType", "UserRegistered");
+
+            messagesToSend.Add(message);
+
+            Console.WriteLine($"  - Prepared message for {user.Email}");
+        }
+
+        try
+        {
+            Console.WriteLine("\nSending messages to the topic...");
+            foreach (var message in messagesToSend)
+            {
+                await sender.SendMessageAsync(message);
+            }
+            Console.WriteLine($"Successfully sent {messagesToSend.Count} messages.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+
+}
